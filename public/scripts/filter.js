245da97a394e15e08f9a56e1a97e17050b44ef50
Jonatha Gomes <jonatha.dev.js@gmail.com>
2021-04-14 18:00:22 -0300

    Adicionando filtros na aplicação

---
+++ public/scripts/filter.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".list").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".card").show("1000");
+    } else {
+      $(".card")
+        .not("." + value)
+        .hide("1000");
+      $(".card")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+  $(".list").click(function () {
+    $(this).removeClass("active");
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+});
